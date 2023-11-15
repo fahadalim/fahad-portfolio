b857982a3a632b8b98f7ddf2aede540fc764336f
Mohammad Fahad Alim <97460449+fahadalim@users.noreply.github.com>
2023-11-15 23:46:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import React from 'react';
 import { useEffect, useContext } from "react";
 import { ThemeContext } from "./context/ThemeContext";
 import Landing from "./Components/Landing/Landing";
